7e368cee1c444b645c1869e811a6b109fc3bea67
simple <86154358+simple-shadow@users.noreply.github.com>
2024-01-01 14:53:14 +0800

    Update SelfSignCert.sh

--- SelfSignCert.sh
+++ SelfSignCert.sh
@@ -6,8 +6,8 @@ domain_name="bing.com"
 # 生成私钥
 openssl genrsa -out ${domain_name}.key 2048
 
-# 生成自签名证书
-openssl req -x509 -new -key ${domain_name}.key -out ${domain_name}.crt -days 365 -subj "/C=CN/ST=Beijing/L=Haidian/O=Acme Corporation/OU=IT Department/CN=${domain_name}"
+# 生成自签名证书，并使用 SAN 字段
+openssl req -x509 -new -key ${domain_name}.key -out ${domain_name}.crt -days 365 -subj "/C=CN/ST=Beijing/L=Haidian/O=Acme Corporation/OU=IT Department/CN=${domain_name}" -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${domain_name}"))
 
 # 创建包含密钥和证书的 PEM 文件
 openssl pkcs12 -export -in ${domain_name}.crt -inkey ${domain_name}.key -out ${domain_name}.pem -password pass:changeit
